075ea5da4ae3a9d7c5073670919fa110c8ea3b8b
Thibault Jeandet <tjeandet@broadinstitute.org>
2018-02-25 15:06:32 -0500

    fix queue metric

--- services/src/main/scala/cromwell/services/loadcontroller/impl/MetadataQueueMetric.scala
+++ services/src/main/scala/cromwell/services/loadcontroller/impl/MetadataQueueMetric.scala
@@ -1,7 +1,7 @@
 package cromwell.services.loadcontroller.impl
 
 case object MetadataQueueMetric extends LoadMetric {
-  val highLoadRange: Range = Range(500 * 1000, 75)
-  val veryHighLoadRange: Range = Range(75, 90)
-  val criticalLoadRange: Range = Range(90, 100)
+  val highLoadRange: Range = Range(100 * 1000, 500 * 1000)
+  val veryHighLoadRange: Range = Range(500 * 1000, 1 * 1000 * 1000)
+  val criticalLoadRange: Range = Range(1 * 1000 * 1000, Int.MaxValue)
 }
